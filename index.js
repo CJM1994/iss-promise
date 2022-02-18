838e09153b0e10b343022a6f4f993e2496dc6ddc
CJM1994 <connorjohnmullin@gmail.com>
2022-02-18 10:24:44 -0800

    made new v with promises

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const request = require('request-promise-native');
+const { nextISSTimesForMyLocation, printISSTimes } = require('./iss');
 
-const fetchMyIP = function () {
-  console.log('hello')
-  return request('https://api.ipify.org?format=json');
-};
-
-module.exports = fetchMyIP;
+nextISSTimesForMyLocation()
+  .then(passTimes => {
+    printISSTimes(passTimes);
+  });
--- iss.js
+++ iss.js
@@ -1,4 +1,38 @@
-const fetchMyIP = require('./iss');
+const request = require('request-promise-native');
 
-fetchMyIP()
-  .then(body => console.log(body));
+const fetchMyIP = function () {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function (body) {
+  const IP = JSON.parse(body).ip;
+  return request(`https://api.freegeoip.app/json/${IP}?apikey=a74512d0-906b-11ec-9ae3-8368081b37ef`);
+};
+
+const fetchISSFlyOverTimes = function (body) {
+  const { latitude, longitude } = JSON.parse(body);
+  return request(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`)
+};
+
+const nextISSTimesForMyLocation = function () {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+const printISSTimes = function (data) {
+
+  for (ISSpass of data) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(ISSpass.risetime);
+    const duration = ISSpass.duration;
+    console.log(`The ISS will pass next on ${dateTime} for a duration of ${duration} seconds`);
+  };
+
+}
+
+module.exports = { nextISSTimesForMyLocation, printISSTimes };
